1aa32a9adcae5ed63226d82e49c3e2d8b87b24e9
ponomarovs87 <ponomarovs87@gmail.com>
2023-03-20 12:22:19 +0100

    Fix error with type input

--- game/index.js
+++ game/index.js
@@ -26,11 +26,11 @@ function main() {
 
     while (hero.isAlive()) {
         console.log("Select defence part:\n1. HEAD\n2. BODY\n3. LEGS");
-        const defence = promt()
+        const defence = +promt()
         hero.chooseDefence(defence)
 
         console.log("Select attack point:\n1. HEAD\n2. BODY\n3. LEGS");
-        const attack = promt()
+        const attack = +promt()
         hero.chooseAttackPart(attack)
 
         monster.chooseAttackPart(getRandomNumber(3));
